d33e9cf09c22a084a622ae5afb6e1fafe01b4eaf
Maxim Molochkov <klaseca@gmail.com>
2021-01-23 17:49:11 +0400

    Upgrade mobx to v6

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,53 +1,54 @@
-import { configure, observable, action, computed, runInAction } from 'mobx';
+import { makeAutoObservable, computed, runInAction } from 'mobx';
 
-configure({ enforceActions: 'always' });
-
-const store = observable(
-  {
-    paths: [],
-    contents: [],
-    async getDirs() {
-      const res = await fetch('/content');
-      const json = await res.json();
-      runInAction(() => {
-        this.contents = json;
-      });
-    },
-    async getContent({ target: { textContent } }) {
-      const parametr = this.paths.length
-        ? `${this.paths.join('/')}/${textContent}`
-        : textContent;
-      const parametrEncode = encodeURIComponent(parametr);
-      const res = await fetch(`/content/${parametrEncode}`);
-      const json = await res.json();
-      runInAction(() => {
-        this.contents = json;
-        this.paths.push(textContent);
-      });
-    },
-    async getDownContent(index) {
-      this.paths.splice(-index);
-      const parametr = `${this.paths.join('/')}`;
-      const parametrEncode = encodeURIComponent(parametr);
-      const res = await fetch(`/content/${parametrEncode}`);
-      const json = await res.json();
-      runInAction(() => {
-        this.contents = json;
-      });
-    },
-    downloadLink(name, ext = '') {
-      return computed(() => {
-        const parametr = `${this.paths.join('/')}/${name}`;
-        const parametrEncode = encodeURIComponent(parametr);
-        return `http://${document.location.hostname}:3232/download${ext}/${parametrEncode}`;
-      });
-    },
-  },
-  {
-    getDirs: action,
-    getContent: action,
-    getDownContent: action,
+class Store {
+  constructor() {
+    makeAutoObservable(this);
   }
-);
+
+  paths = [];
+  contents = [];
+
+  getDirs = async () => {
+    const res = await fetch('/content');
+    const json = await res.json();
+    runInAction(() => {
+      this.contents = json;
+    });
+  };
+
+  getContent = async ({ target: { textContent } }) => {
+    const parametr = this.paths.length
+      ? `${this.paths.join('/')}/${textContent}`
+      : textContent;
+    const parametrEncode = encodeURIComponent(parametr);
+    const res = await fetch(`/content/${parametrEncode}`);
+    const json = await res.json();
+    runInAction(() => {
+      this.contents = json;
+      this.paths.push(textContent);
+    });
+  };
+
+  getDownContent = async (index) => {
+    this.paths.splice(-index);
+    const parametr = `${this.paths.join('/')}`;
+    const parametrEncode = encodeURIComponent(parametr);
+    const res = await fetch(`/content/${parametrEncode}`);
+    const json = await res.json();
+    runInAction(() => {
+      this.contents = json;
+    });
+  };
+
+  downloadLink = (name, ext = '') => {
+    return computed(() => {
+      const parametr = `${this.paths.join('/')}/${name}`;
+      const parametrEncode = encodeURIComponent(parametr);
+      return `http://${document.location.hostname}:3232/download${ext}/${parametrEncode}`;
+    }).get();
+  };
+}
+
+const store = new Store();
 
 export default store;
