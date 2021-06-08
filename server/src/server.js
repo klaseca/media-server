05320ba7c38d1c97f2e3edf8199f08a4d5f9ed3c
Maxim Molochkov <klaseca@gmail.com>
2021-06-08 21:34:44 +0400

    Import dotenv config directly

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,6 @@
-import dotenv from 'dotenv'
+import 'dotenv/config';
 import { createServer } from 'http';
 import app from 'app';
-
-dotenv.config();
 
 createServer(app.callback()).listen(process.env.PORT, () => {
   console.log('Server started, UwU');
