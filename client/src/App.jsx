2af610026163cd7976b48e2e250cfecabb70ba2f
Maxim Molochkov <klaseca@gmail.com>
2021-08-12 17:21:23 +0400

    Use correct name of component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Flex } from '@chakra-ui/core';
-import BreadcrumbCstm from 'components/BreadcrumbCstm';
+import BreadcrumbRouter from 'components/BreadcrumbRouter';
 import Container from 'components/Container';
 import ContentBox from 'components/ContentBox';
 
 const App = () => (
   <>
-    <BreadcrumbCstm />
+    <BreadcrumbRouter />
     <Flex bg='gray.700' flexDir='column' color='gray.200' flexGrow='1'>
       <Container>
         <ContentBox />
