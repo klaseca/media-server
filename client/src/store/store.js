82d5256d3bfba8b04b9b38ac923f60a4d694785b
Maxim Molochkov <klaseca@gmail.com>
2021-08-12 22:14:28 +0400

    Remake store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,54 +1,31 @@
 import { makeAutoObservable, computed, runInAction } from 'mobx';
 
-class Store {
+export class Store {
   constructor() {
     makeAutoObservable(this);
   }
 
-  paths = [];
   contents = [];
 
-  getDirs = async () => {
-    const res = await fetch('/content');
-    const json = await res.json();
-    runInAction(() => {
-      this.contents = json;
-    });
+  prepareContentPath = (path) => {
+    if (path === '/') {
+      return '/content';
+    }
+    const pathEncode = encodeURIComponent(path.slice(1));
+    return `/content/${pathEncode}`;
   };
 
-  getContent = async ({ target: { textContent } }) => {
-    const parametr = this.paths.length
-      ? `${this.paths.join('/')}/${textContent}`
-      : textContent;
-    const parametrEncode = encodeURIComponent(parametr);
-    const res = await fetch(`/content/${parametrEncode}`);
-    const json = await res.json();
+  loadContent = async (contentPath) => {
+    const res = await fetch(this.prepareContentPath(contentPath));
+    const contents = await res.json();
     runInAction(() => {
-      this.contents = json;
-      this.paths.push(textContent);
-    });
-  };
-
-  getDownContent = async (index) => {
-    this.paths.splice(-index);
-    const parametr = `${this.paths.join('/')}`;
-    const parametrEncode = encodeURIComponent(parametr);
-    const res = await fetch(`/content/${parametrEncode}`);
-    const json = await res.json();
-    runInAction(() => {
-      this.contents = json;
+      this.contents = contents;
     });
   };
 
   downloadLink = (name, ext = '') => {
-    return computed(() => {
-      const parametr = `${this.paths.join('/')}/${name}`;
-      const parametrEncode = encodeURIComponent(parametr);
-      return `http://${document.location.hostname}:3232/download${ext}/${parametrEncode}`;
-    }).get();
+    return computed(
+      () => `http://${document.location.hostname}:3232/download${ext}/${name}`
+    ).get();
   };
 }
-
-const store = new Store();
-
-export default store;
