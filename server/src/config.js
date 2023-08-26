7f2b325022397d8eff2c3d07cc1f2288b1d5f7f5
Maxim Molochkov <klaseca@gmail.com>
2023-08-26 16:33:35 +0400

    Config server with env

--- server/src/config.js
+++ server/src/config.js
@@ -1,10 +1,5 @@
 export const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3232,
-  publicDirs: [
-    {
-      path: 'path/to/dir',
-      alias: 'Your alias to path',
-    },
-  ],
+  publicDirs: JSON.parse(process.env.PUBLIC_DIRS),
 };
